0a6fd4c00c8c7538a3d7d7efa23bfafebc431e0b
BVGdragon1025 <72506695+BVGdragon1025@users.noreply.github.com>
2020-12-14 16:53:58 +0100

    Zadanie 3

---
+++ Cwiczenia11/cwiczenia3.py
@@ -0,0 +1,9 @@
+text1 = f"""Długo na szturm i szaniec poglądał w milczeniu. Na
+koniec rzekł: 'Stracona' """
+
+lista = ["Zwinny", "lis", "przeskoczył", "nad", "leniwym", "psem", "."]
+
+print(text1[0:(text1.find(".")+1)])
+
+text2 = " ".join(lista)
+print(text2)

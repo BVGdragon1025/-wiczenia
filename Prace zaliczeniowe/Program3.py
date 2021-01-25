df17510440cef531f9f4159c0ee759f2c03329c5
BVGdragon1025 <72506695+BVGdragon1025@users.noreply.github.com>
2021-01-25 21:32:55 +0100

    Aktualizacja zadań zaliczeniowych oraz uporządkowanie folderu "ćwiczenia12"

--- Prace zaliczeniowe/Program3.py
+++ Prace zaliczeniowe/Program3.py
@@ -1,2 +1,108 @@
+# Na ten moment popsute jest sprawdzanie czy pytanie już było. Właściwie działa, ale resetuje licznik.
+# Do poprawki
+
 import random
 
+# Krotka z pytaniami
+qstns = ("Rozwiń skrót HTML",
+         "Rozwiń skrót CSS",
+         "Rozwiń skrót JS",
+         "W jaki sposób zapiszesz szesnastkowo kolor czarny",
+         "W jaki sposób zapiszesz szesnastkowo kolor biały",
+         "Językiem czego jest HTML?",
+         "Jaką komendą można sprawdzić zawartość pliku w Linuxie?",
+         "Jakim typem języka jest Python?",
+         "Czy C++ jest językiem kompilowanym?",
+         "Jakie słowo definiuje funkcję w Pythonie?",
+         "Jakie rozszerzenie ma plik JavaScript? (bez kropki)",
+         "Jakie rozszerzenie ma plik C++? (bez kropki)",
+         "Czy C++ to język interpretowany?",
+         "Jak nazywa się język ezoteryczny zainspirowany memiczną "
+         "mową kotów?",
+         "Czy Python to język interpretowany?",
+         "Czy Python to język kompilowany?",
+         "28"
+         )
+
+# Krotka z odpowiedziami
+answrs = ("Hypertext Markup Language",
+          "Cascade Style Sheet",
+          "JavaScript",
+          "000000",
+          "FFFFFF",
+          "Znaczników",
+          "cat",
+          "Interpretowanym",
+          "Tak",
+          "def",
+          "js",
+          "cpp",
+          "Nie",
+          "LOLCAT",
+          "Tak",
+          "Nie",
+          "28"
+          )
+
+# Tablica z wartościami, które już były
+repeat = []
+
+
+def main():
+    print("Witaj w quizie!")
+    print("Pamiętaj, aby odpowiedzi na pytania były napisane poprawnie")
+    choice = input("Czy jesteś gotów? Tak lub Nie: ")
+    if choice == "Tak" or choice.capitalize() == "Tak" or choice.lower() == "tak":
+        quiz()
+        grade(quiz())
+    else:
+        print("Widzimy się więc wkrótce!")
+
+
+def quiz():
+    # Licznik punktów
+    points = 0
+    for i in range(10):
+        quest = random.randint(0, len(qstns)-1)
+        # Sprawdzenie czy pytanie już było
+        if quest in repeat:
+            quest = random.randint(0, len(qstns) - 1)
+        else:
+            repeat.append(quest)
+            # Wyświetlenie pytania i zapytanie usera
+            print(qstns[quest])
+            answer = input("Podaj odpowiedź: ")
+            # Sprawdzenie poprawności odpowiedzi
+            if answer.capitalize() == answrs[quest]\
+                    or answer == answrs[quest]\
+                    or answer.upper() == answrs[quest]:
+                # Dodanie punktu
+                print("Dobra odpowiedź!, +1")
+                points += 1
+                print(points)
+                print(repeat)
+            else:
+                print(points)
+                print(repeat)
+                print("Zła odpowiedź, lecimy dalej")
+    return points
+
+
+# Podliczenie punktów i wystawienie oceny
+def grade(points):
+    print(points)
+    if points <= 3:
+        print("Ocena niedostateczna. Powodzenia następnym razem")
+    elif 3 < points <= 5:
+        print("Ocena dostateczna. Mogło być lepiej, ale zaliczyłeś!")
+    elif 5 < points <= 7:
+        print("Ocena dostateczna. Może być!")
+    elif 7 < points <= 8:
+        print("Ocena dobra. Nieźle się spisałeś!")
+    elif 8 < points <= 9:
+        print("Ocena bardzo dobra. Świetna robota!")
+    else:
+        print("Ocena celująca. Rozwaliłeś ten test, gratulacje!")
+
+
+main()
--- Prace zaliczeniowe/Program5.py
+++ Prace zaliczeniowe/Program5.py
@@ -11,7 +11,19 @@
 # 1 korona czeska to 0.047 dolara
 # 1 monero to 139.44 dolarów
 
-def menu():
+
+# Krotka z wartościami poszczegolnych walut
+tuple_conv = (0.033, 32368.00, 0.014, 0.028, 1256.48,
+              0.00856, 0.00043, 1, 0.047, 139.44)
+
+# Krotka z nazwami walut
+tuple_names = ("Baht", "Bitcoin", "Ngultrum", "Ugija",
+               "Etherum", "Dogecoin", "boliwar wenezuelski (Dong)",
+               "panamski balboa", "koron czeskich",
+               "monero")
+
+
+def menu():  # Menu dla użytkownika
     print("Wybierz sposób przeliczania: ")
     print("[1] Dolary na inną walutę: ")
     print("[2] Inna waluta na dolary: ")
@@ -19,14 +31,42 @@ def menu():
     return choice
 
 
-def wybor(choice):
-    menu()
+def wybor(choice):  # Wybor typu przeliczenia
+
     if choice == "1":
         print("Na jaką walutę: ")
+        print_choices()
         choice2 = str(input("Wybierz: "))
+        conversion(choice, choice2)
     else:
         print("Jaka waluta")
+        print_choices()
         choice2 = str(input("Wybierz: "))
+        conversion(choice, choice2)
 
 
-wybor(menu)
+def print_choices():  # Wydruk wyborów, aby oszczędzić linijki
+    print("[1] Baht Tajski ")
+    print("[2] Bitcoin ")
+    print("[3] Ngultrum ")
+    print("[4] Ugija ")
+    print("[5] Etherum ")
+    print("[6] Dogecoin ")
+    print("[7] Boliwar Wenezuelski (Dong) ")
+    print("[8] Panamski Balboa ")
+    print("[9] Korona czeska ")
+    print("[10] Monero ")
+
+
+def conversion(choice, choice2):  # Konwersja walut na podstawie wyboru sposobu konwersji
+    if choice == "1":
+        dolar = int(input("Podaj ile dolarów: "))
+        conv = dolar / tuple_conv[int(choice2)-1]
+        print(dolar, "$ to", round(conv, 2), tuple_names[int(choice2)-1])
+    elif choice == "2":
+        other = int(input("Podaj ile: "))
+        conv = other * tuple_conv[int(choice2) - 1]
+        print(other, tuple_names[int(choice2) - 1], "to", round(conv, 2), "$")
+
+
+wybor(menu())

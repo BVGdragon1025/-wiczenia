c494763c99730631d6e466af12064153c4d4f80f
BVGdragon1025 <72506695+BVGdragon1025@users.noreply.github.com>
2021-05-10 11:46:32 +0200

    Zadania z dzisiaj

--- Semestr 2/Cwiczenia 9/cwiczenie 3.py
+++ Semestr 2/Cwiczenia 9/cwiczenie 3.py
@@ -20,17 +20,16 @@ part1()
 
 
 def part2():
-    kolejka = []
+    kolejka2 = []
     with open("losowe_liczby.txt", "w+") as writenumbers:
         for i in range(50):
             writenumbers.write(str(random.randrange(0, 100)) + "\n")
 
     with open("losowe_liczby.txt", "r") as readnumbers:
         print("Liczby w pliku: ", readnumbers.read().rsplit("\n"))
-        temp = readnumbers.readlines()
         for numbers in readnumbers:
-            kolejka.append(numbers)
-        print("Kolejka: ", kolejka)
+            kolejka2.append(numbers.rsplit("\n"))
+        print("Kolejka: ", kolejka2)
 
 
 part2()
